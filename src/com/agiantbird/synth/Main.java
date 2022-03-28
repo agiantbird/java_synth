b250a2c1ae63a5a480d9f441e1e4ffe81671ecc5
Rich Hoyt <rhoyt10@gmail.com>
2022-03-28 09:37:37 -0400

    Add synthesizer class

--- src/com/agiantbird/synth/Main.java
+++ src/com/agiantbird/synth/Main.java
@@ -2,6 +2,6 @@ package com.agiantbird.synth;
 
 public class Main {
     public static void main(String[] args) {
-
+        new Synthesizer();
     }
 }
---
+++ src/com/agiantbird/synth/Synthesizer.java
@@ -0,0 +1,16 @@
+package com.agiantbird.synth;
+
+import javax.swing.*;
+
+public class Synthesizer {
+    private JFrame frame = new JFrame("Syntheziser");
+
+    Synthesizer() {
+        frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        frame.setSize(613, 357);
+        frame.setResizable(false);
+        frame.setLayout(null);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
